7e294108fc12c78da38e893c37f14872d74b897b
Anders Borum <anders@algoritmer.dk>
2017-06-22 00:21:35 +0200

    security scoped url's are persisted across calls

--- OpenInPlace/ListController.swift
+++ OpenInPlace/ListController.swift
@@ -10,16 +10,22 @@ import UIKit
 import MobileCoreServices
 
 class ListController: UITableViewController, UIDocumentPickerDelegate {
-
     var detailViewController: EditController? = nil
-    var objects = [Any]()
+    
+    // security scoped URL's are shown when nil, otherwise the contents of directory
+    public var baseURL: URL? = nil
+    
+    var urls = [URL]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        restoreUrlBookmarks()
+        
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pickURLs(_:)))
         navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
@@ -27,29 +33,19 @@ class ListController: UITableViewController, UIDocumentPickerDelegate {
         }
     }
     
-    @objc
-    func insertNewObject(_ sender: Any) {
-        
-        let types = [kUTTypeText as String, kUTTypeDirectory as String]
-        let picker = UIDocumentPickerViewController(documentTypes: types, in: .open)
-                
-        if #available(iOS 11.0, *) {
-            picker.allowsMultipleSelection = true
-        }
-        picker.delegate = self
-        present(picker, animated: true, completion: nil)
-    }
-
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! EditController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
+                let url = urls[indexPath.row]
+                
+                
+                
+                //let controller = (segue.destination as! UINavigationController).topViewController as! EditController
+                //controller.detailItem = object
+                //controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                //controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
@@ -61,14 +57,18 @@ class ListController: UITableViewController, UIDocumentPickerDelegate {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return urls.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let url = urls[indexPath.row]
+        if(url.startAccessingSecurityScopedResource()) {
+            cell.textLabel!.text = url.lastPathComponent
+            url.stopAccessingSecurityScopedResource()
+        }
+        
         return cell
     }
 
@@ -79,18 +79,92 @@ class ListController: UITableViewController, UIDocumentPickerDelegate {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            urls.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
     
-    //MARK: UIDocumentPickerDelegate
+    // MARK: -
     
+    private let bookmarksDefaultsKey = "bookmarks"
     
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+    // to be able to save and restore security scoped URL's these must be stored as bookmarks
+    func saveUrlBookmarks() {
+        guard baseURL == nil else {
+            return
+        }
         
+        var bookmarks = [Data]()
+        
+        for url in urls {
+            guard url.startAccessingSecurityScopedResource()
+            else { continue }
+            
+            do {
+                let bookmark = try url.bookmarkData(options: [.suitableForBookmarkFile],
+                                                    includingResourceValuesForKeys: nil,
+                                                    relativeTo: nil)
+                bookmarks.append(bookmark)
+                
+            } catch {
+                print("\(error)")
+            }
+            
+            url.stopAccessingSecurityScopedResource()
+        }
+        
+        // store in user defaults, since this is just a demo
+        UserDefaults.standard.set(bookmarks, forKey: bookmarksDefaultsKey)
+    }
+    
+    func restoreUrlBookmarks() {
+        guard baseURL == nil else {
+            return
+        }
+        
+        var newUrls = [URL]()
+        
+        let bookmarks = UserDefaults.standard.object(forKey: bookmarksDefaultsKey) as? [Data]
+        if(bookmarks != nil) {
+            for bookmark in bookmarks! {
+                
+                do {
+                    var stale = false
+                    let url = try URL(resolvingBookmarkData: bookmark, bookmarkDataIsStale: &stale)
+                    if(url != nil) {
+                        newUrls.append(url!)
+                    }
+                    
+                } catch {
+                    print("\(error)")
+                }
+                
+            }
+        }
+        urls = newUrls
+    }
+    
+    @objc func pickURLs(_ sender: Any) {
+        
+        let types = [kUTTypeText as String, kUTTypeDirectory as String]
+        let picker = UIDocumentPickerViewController(documentTypes: types, in: .open)
+        
+        if #available(iOS 11.0, *) {
+            picker.allowsMultipleSelection = true
+        }
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //MARK: - UIDocumentPickerDelegate
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt newUrls: [URL]) {
+        urls.append(contentsOf: newUrls)
+        saveUrlBookmarks()
+        
+        tableView.reloadData()
     }
     
     // this is called on iOS versions before 11 and we just pass URL along in array
